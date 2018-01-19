211ebcc86ddf444cdd703fc9d4bdcc282a85de61
Luis Gonzalez <luisgonzalez@Luiss-MacBook-Pro.local>
2018-01-18 18:02:54 -0600

    second commit

--- src/practica/Practica.java
+++ src/practica/Practica.java
@@ -10,53 +10,61 @@ package practica;
  * @author luisgonzalez
  */
 public class Practica {
-    
-    private int numeros;
-    
-     public void ContadorFor(int x){
-         
-         int resultado = 0;
-         
-         for(int i=0; i<x; x--){
+
+    private int resultado = 0;
+
+    public void ContadorFor(int x) {
+
+        int resultado = 0;
+
+        for (int i = 0; i < x; x--) {
             resultado = resultado + x;
-         }
-         
-         System.out.println("El resultado final es: " + resultado);
-  
-     }
-     
-     public void ContadorWhile(int x){
-         
-         int resultado = 0;
-         
-         while(x>0){
+        }
+
+        System.out.println("El resultado final es: " + resultado);
+
+    }
+
+    public void ContadorWhile(int x) {
+
+        int resultado = 0;
+
+        while (x > 0) {
             resultado += x;
             x--;
-            System.out.println(resultado);
-         }
-         
-         System.out.println("El resultado final es: " + resultado);
-         
-     }
-     
-     public void ContadorRecursivo(int x){
-         
-     }
+        }
 
+        System.out.println("El resultado final es: " + resultado);
+
+    }
+
+    public void ContadorRecursivo(int x) {
+
+        if (x == 0) {
+            System.out.println("El resultado final es: " + resultado);
+        } else {
+            resultado += x;
+            x = x - 1;
+            ContadorRecursivo(x);
+        }
+        
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
         Practica instanciaPractica = new Practica();
+        
         ejercicios instanciaEjercicios = new ejercicios();
-        
-       instanciaPractica.ContadorWhile(5);
-       //instanciaEjercicios.ContadorFor(5);
-        
-        
+
+        instanciaPractica.ContadorFor(5);
+        instanciaPractica.ContadorWhile(5);
+        instanciaPractica.ContadorRecursivo(5);
+        //instanciaEjercicios.ContadorFor(5);
+
     }
-    
+
 }
